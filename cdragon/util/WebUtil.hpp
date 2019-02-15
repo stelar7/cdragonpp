80bd846b41698b037ea772f9d5f54b825cdcc58d
stelar7 <dudedbz@gmail.com>
2019-02-15 23:02:37 +0100

    Add error message about file downloading Fix building on x86

--- cdragon/util/WebUtil.hpp
+++ cdragon/util/WebUtil.hpp
@@ -1,5 +1,7 @@
 #include "../../libs/curl/include/curl.h"
 #include <iostream>
+#include <string.h>
+#include <errno.h>
 
 namespace cdragon {
     namespace web {
@@ -38,16 +40,27 @@ namespace cdragon {
             throw std::exception("Failed to read from url!");
         }
 
+        /*
+        This needs the parent directory to already exist.
+        Folder creation is not included in this function, as it is platform specific.
+        */
         bool downloadFile(std::string url, std::string output) {
             CURL* curl;
             CURLcode res;
             FILE* fp;
             bool status = true;
 
+            errno_t err = fopen_s(&fp, output.c_str(), "wb");
+            if (err != 0) {
+                size_t errLen = 265;
+                char errmsg[266];
+                strerror_s(errmsg, errLen, err);
+                std::cout << errmsg << std::endl;
+                return false;
+            }
+
             curl = curl_easy_init();
             if (curl) {
-                fopen_s(&fp, output.c_str(), "wb");
-
                 curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
                 curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
                 curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
